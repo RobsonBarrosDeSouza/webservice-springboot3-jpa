80c10e19ab0c81ebada6255bd35602aa8b3bdc38
Robson Barros <robsonhx@hotmail.com>
2023-07-11 22:29:05 -0300

    pt 1

--- src/main/java/med/voll/api/entities/User.java
+++ src/main/java/med/voll/api/entities/User.java
@@ -1,12 +1,15 @@
 package med.voll.api.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String passorwd;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Oder> orders = new ArrayList<>();
 	
 	public User() {
 	}
